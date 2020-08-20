63f67c7727f766e245ac7ae44b1201f0df00fc5f
Michal Perlak <michalperlak@windowslive.com>
2020-08-20 20:58:37 +0200

    Project init

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "dev.talkischeap"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
