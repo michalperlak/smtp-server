8a891305856683da90f37a5e3f1c9a84edc52c7c
Michal Perlak <michalperlak@windowslive.com>
2020-08-23 13:25:59 +0200

    Extended hello command

--- src/main/kotlin/dev/talkischeap/smtp/server/commands/ExtendedHelloCommand.kt
+++ src/main/kotlin/dev/talkischeap/smtp/server/commands/ExtendedHelloCommand.kt
@@ -1,9 +1,15 @@
 package dev.talkischeap.smtp.server.commands
 
-class ExtendedHelloCommand: Command {
+import dev.talkischeap.smtp.server.constants.SmtpStatus
+
+class ExtendedHelloCommand(
+        private val serverHost: String
+): Command {
     override val name: String = "EHLO"
 
-    override fun execute(arguments: CommandArguments): CommandResponse {
-        TODO("Not yet implemented")
-    }
+    override fun execute(arguments: CommandArguments): CommandResponse =
+            when {
+                arguments.empty() -> CommandResponse(SmtpStatus.PARAMETERS_SYNTAX_ERROR, "Syntax: EHLO <hostname>")
+                else -> CommandResponse(SmtpStatus.REQUESTED_ACTION_COMPLETED, serverHost)
+            }
 }
