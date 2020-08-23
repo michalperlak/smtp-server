3c671686fc6cdc92487bae264e2eb463dff79d19
Michal Perlak <michalperlak@windowslive.com>
2020-08-23 13:30:38 +0200

    Quit command

--- src/main/kotlin/dev/talkischeap/smtp/server/constants/SmtpStatus.kt
+++ src/main/kotlin/dev/talkischeap/smtp/server/constants/SmtpStatus.kt
@@ -3,6 +3,7 @@ package dev.talkischeap.smtp.server.constants
 enum class SmtpStatus(
         val code: Int
 ) {
+    SERVER_CLOSING_TRANSMISSION_CHANNEL(221),
     REQUESTED_ACTION_COMPLETED(250),
     PARAMETERS_SYNTAX_ERROR(501)
 }
