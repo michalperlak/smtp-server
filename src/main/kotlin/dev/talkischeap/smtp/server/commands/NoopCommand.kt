694a10b45e8f8592534ca4e2a9bd06ae09e14fdc
Michal Perlak <michalperlak@windowslive.com>
2020-08-23 13:28:57 +0200

    Noop command

--- src/main/kotlin/dev/talkischeap/smtp/server/commands/NoopCommand.kt
+++ src/main/kotlin/dev/talkischeap/smtp/server/commands/NoopCommand.kt
@@ -1,9 +1,10 @@
 package dev.talkischeap.smtp.server.commands
 
+import dev.talkischeap.smtp.server.constants.SmtpStatus
+
 class NoopCommand: Command {
     override val name: String = "NOOP"
 
-    override fun execute(arguments: CommandArguments): CommandResponse {
-        TODO("Not yet implemented")
-    }
+    override fun execute(arguments: CommandArguments): CommandResponse =
+            CommandResponse(SmtpStatus.REQUESTED_ACTION_COMPLETED, "OK")
 }
